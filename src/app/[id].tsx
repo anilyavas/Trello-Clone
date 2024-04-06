90ba83ceec0189b45ff310def2551fdaef1cc647
anilyavas <anil.yavas99@gmail.com>
2024-04-06 15:05:13 +0300

    update, delete and write task

--- src/app/[id].tsx
+++ src/app/[id].tsx
@@ -1,23 +1,38 @@
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { View, Text, StyleSheet } from 'react-native';
-import { useObject } from '@realm/react';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { useObject, useRealm } from '@realm/react';
 import { Task } from '../modals/Task';
 import { BSON } from 'realm';
+import { useState } from 'react';
 
 const TaskDetails = () => {
   const { id } = useLocalSearchParams();
   const task = useObject<Task>(Task, new BSON.ObjectId(id as string));
+  const [updatedDescription, setUpdatedDescription] = useState(
+    task?.description
+  );
+  const realm = useRealm();
 
   if (!task) {
     return <Text>Not found</Text>;
   }
+  const updateDescription = () => {
+    if (!task) {
+      return;
+    }
+    realm.write(() => {
+      task.description = updatedDescription;
+    });
+  };
   return (
     <View style={styles.container}>
       <Stack.Screen options={{ title: 'Task Details' }} />
-      <Text style={styles.text}>
-        id: {id}
-        {task.description}
-      </Text>
+      <TextInput
+        value={updatedDescription}
+        onChangeText={setUpdatedDescription}
+        onEndEditing={updateDescription}
+        style={styles.text}
+      />
     </View>
   );
 };
